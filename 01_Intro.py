3db078aaca961e78b9c76cfc53c4da4cec7d02a2
Jonas Eyskens <JonasEyskens@Pay2Play.local>
2017-09-26 11:34:51 +0200

    Initial commit

---
+++ 01_Intro.py
@@ -0,0 +1,50 @@
+# 01 Basis variabelen
+
+# print("Hellow World, dag")
+# print(1+1)
+
+# naam = input("Geef mij jouw naam")
+# voornaam = input("Geef mij jouws voornaam")
+# leeftijd = input("Geef mij jouw leeftijd") # is een string
+
+# datatype conversie : int, float, ...
+# leeftijd = int(leeftijd) + 1
+
+# print("Mijn naam is {0} {1} en ik ben {2} jaar oud.".format(naam , voornaam , leeftijd))
+
+
+
+
+# Oefening oppervlakte driehoek
+
+# basis = input("Geef mij de basis van de driehoek: ")
+# basis = float(input("Geef de mij de basis van de driehoek: "))
+# hoogte = input("Geef mij de hoogte van de driehoek: ")
+# hoogte = float(input("Geef mij de hoogte van de driehoek: "))
+# oppervlakte = basis * hoogte /2
+# print("De oppervlakte van de driehoek is: {0:.2f}".format(oppervlakte))
+
+
+# Oefening Dagen,Uren,Minuten,Seconden
+
+#dag = int(input("Geef het aantal dagen op: "))
+#dagSec = dag * 24 * 60 * 60
+#uur = int(input("Geef het aantal uren op: "))
+#uurSec = uur * 60 * 60
+#min = int(input("Geef het aantal minuten op: "))
+#minSec = min * 60
+#sec = int(input("Geef het aantal seconden op: "))
+#totaal = dagSec + uurSec + minSec + sec
+#print("Het totale aantal seconden bedraagt:", totaal)
+
+
+#Oefening seconden naar tijd
+
+#seconden = int(input("Geef het aantal seconden op: "))
+
+
+#Oefening som: n + nn + nnn
+
+n = input("Geef een getal op: ")
+som = n + nn + nnn
+print("Het resultaat is: ", som)
